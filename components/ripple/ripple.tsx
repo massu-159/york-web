151dddbf0604e423863ddce20c9a13380269cdbe
massu-159 <magjtwjp@gmail.com>
2025-08-01 22:13:33 +0900

    [update] refine hero UX

--- components/ripple/ripple.tsx
+++ components/ripple/ripple.tsx
@@ -7,11 +7,14 @@ import R3FCard from './r3f-components/Card';
 const HtmlCard = () => {
   return (
     <>
-      <img
-        id='target-image'
-        className='absolute top-0 left-0 object-cover w-full h-full'
-        src={PLACEHOLDER_IMAGE_URL}
-      />
+      <div id='target-image' className='absolute top-0 left-0 w-full h-full'>
+        <div className='absolute inset-0 flex items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100'>
+          <div className='relative'>
+            <div className='w-12 h-12 border-4 border-gray-300 border-t-pink-600 rounded-full animate-spin'></div>
+            <div className='absolute inset-0 w-12 h-12 border-4 border-transparent border-t-purple-400 rounded-full animate-spin animation-delay-150'></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
@@ -39,7 +42,7 @@ const THREECanvas = () => {
       <EffectComposer />
       {/* <OrbitControls makeDefault /> */}
       <ambientLight />
-      <color attach='background' args={['white']} />
+      <color attach='background' args={['']} />
     </Canvas>
   );
 };
--- components/ui/loading-fallback.tsx
+++ components/ui/loading-fallback.tsx
@@ -1,9 +1,11 @@
 export function LoadingFallback() {
   return (
-    <div className='absolute inset-0 flex items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100'>
-      <div className='relative'>
-        <div className='w-12 h-12 border-4 border-gray-300 border-t-pink-600 rounded-full animate-spin'></div>
-        <div className='absolute inset-0 w-12 h-12 border-4 border-transparent border-t-purple-400 rounded-full animate-spin animation-delay-150'></div>
+    <div className='w-full h-full'>
+      <div className='absolute inset-0 flex items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100'>
+        <div className='relative'>
+          <div className='w-12 h-12 border-4 border-gray-300 border-t-pink-600 rounded-full animate-spin'></div>
+          <div className='absolute inset-0 w-12 h-12 border-4 border-transparent border-t-purple-400 rounded-full animate-spin animation-delay-150'></div>
+        </div>
       </div>
     </div>
   );
